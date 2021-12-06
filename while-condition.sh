2737def753018d0befc0e3699756c4ac5567cecf
BhoirSujit <92661835+BhoirSujit@users.noreply.github.com>
2021-12-06 20:32:36 +0530

    Update while-condition.sh

--- while-condition.sh
+++ while-condition.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-#this program will print 1 to 10 in reverse order
+#while condition
 
 #assign value to variable
-var1=10
+var1=1
 
 #while condition
-while [ $var1 -gt 0 ]
+while [ $var1 -le 10 ]
 do
 	echo $var1
-var1=$[ $var1 - 1] 
+	var1=$[ $var1 + 1 ]
 done
+
+#it will print 1 to 10 numbers
